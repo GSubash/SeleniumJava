07283387d09a66d8da2458c2d087b6e823b9fb8b
Subash <gsubaash@gmail.com>
2022-11-02 10:01:18 +0530

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Test Code to push the first commit to GIT Repository" );
+    }
+}
